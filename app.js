48c65753dc735772a3a458adc73d7b34d8ae1cfc
Michelle <michelle_broomes@yahoo.com>
2020-03-12 15:58:09 -0400

    blah

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 const morgan = require("morgan");
 const express = require("express");
+// const html = require("html-template-tag")
+const layout = require("./views/layout");
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send(layout(' '));
 })
  
  app.use(express.urlencoded({ extended: false }));
  app.use(express.json());
+
+ app.get("/posts/:id", async (req, res, next) => {
+    try {
+    //   const data = await client.query(baseQuery + "WHERE posts.id = $1", [req.params.id]);
+    //   const post = data.rows[0];
+      res.send(layout(content));
+    } catch (error) { next(error) }
+  });
+
  
 
-const PORT = 1337;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`App listening in port ${PORT}`);
